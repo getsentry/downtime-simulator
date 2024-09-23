f00be603867c983a131493b7090e9eca626b7986
Gabriel Lopes <lopes@hey.com>
2024-09-23 13:38:46 -0700

    replace server action with direct querying in page

--- src/app/[host]/page.tsx
+++ src/app/[host]/page.tsx
@@ -3,31 +3,20 @@ import { kv } from "@vercel/kv";
 import { Info } from "lucide-react";
 import { notFound } from "next/navigation";
 import * as Sentry from "@sentry/nextjs";
-import { headers } from "next/headers";
 import { PrismaClient } from "@prisma/client";
+
+async function queryPosts() {
+  const prisma = new PrismaClient();
+  const posts = await prisma.post.findMany();
+
+  return posts;
+}
 
 const Page = async ({ params: { host } }: { params: { host: string } }) => {
   const hostStatus: number | null = await kv.get(`host-status:${host}`);
 
   if (hostStatus === 404) {
     notFound();
-  }
-
-  async function queryPosts() {
-    "use server";
-    return await Sentry.withServerActionInstrumentation(
-      "queryPosts",
-      {
-        headers: headers(),
-        recordResponse: true,
-      },
-      async () => {
-        const prisma = new PrismaClient();
-        const posts = await prisma.post.findMany();
-
-        return posts;
-      }
-    );
   }
 
   if (hostStatus === 500) {
