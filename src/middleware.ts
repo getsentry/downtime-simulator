87001a753c43b2102a40efcc42707e3f9ad8b3dc
Gabriel Lopes <lopes@hey.com>
2024-09-24 15:08:45 -0700

    add logging for request info

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,19 @@ import type { NextRequest } from "next/server";
 const rootHostname = process.env.DOWNTIME_SIMULATOR_ROOT_HOSTNAME;
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
   // continue request to / if root hostname
   if (rootHostname == url.hostname) {
     return NextResponse.next();
   }
+
+  const body = await request.text();
+  console.log("received with request URL", request.url);
+  console.log("received with request method:", request.method);
+  console.log("received with request body:", body);
+  console.log("received with request headers:", request.headers);
 
   return NextResponse.rewrite(new URL(`/${url.hostname}`, request.url), {
     request: request,
